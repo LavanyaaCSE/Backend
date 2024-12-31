75ac792423c7c9fdf6dfe1a1eed7829f167d5661
Lavanyaa A <142470176+LavanyaaCSE@users.noreply.github.com>
2025-01-01 03:08:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,5 +211,10 @@ def get_english_city_news():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/')
+def home():
+    return 'Welcome to the homepage'
+    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
